04d02799a319d0d8663781946b8979a982ae7045
aakash2330 <aakash2330@gmail.com>
2026-05-29 17:06:50 +0530

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+mod accounts;
+use accounts::AccountsManager;
 use std::collections::{BTreeMap, HashMap, VecDeque};
 
-type OrderId = u64;
-type Price = u64;
-type Quantity = u64;
-type UserId = u64;
-type Symbol = String;
-const INITIAL_SYMBOLS: [&str; 1] = ["BTC"];
+#[cfg(test)]
+mod tests;
+
+pub type OrderId = u64;
+pub type Price = u64;
+pub type Quantity = u64;
+pub type UserId = u64;
+pub type Symbol = String;
+pub const INITIAL_SYMBOLS: [&str; 1] = ["BTC"];
 
 #[derive(Debug, Clone, Copy)]
-enum Side {
+pub enum Side {
     Bid,
     Ask,
 }
 #[derive(Debug, Clone)]
-struct Order {
+pub struct Order {
     id: OrderId,
     user_id: UserId,
     side: Side,
@@ -23,7 +28,7 @@ struct Order {
 }
 
 #[derive(Debug)]
-struct Trade {
+pub struct Trade {
     maker_order_id: OrderId,
     taker_order_id: OrderId,
     maker_user_id: UserId,
@@ -38,270 +43,6 @@ struct Trade {
 struct MatchResult {
     resting_order: Option<Order>,
     fills: Vec<Trade>,
-}
-
-#[derive(Debug)]
-struct AccountsManager {
-    accounts: HashMap<UserId, UserAccount>,
-}
-
-#[derive(Debug)]
-struct UserAccount {
-    balance: Price,
-    locked: Price,
-    positions: HashMap<Symbol, UserPosition>,
-}
-
-#[derive(Debug)]
-struct UserPosition {
-    quantity: Quantity,
-    locked: Quantity,
-}
-
-impl UserAccount {
-    fn new() -> Self {
-        let mut positions: HashMap<Symbol, UserPosition> = HashMap::new();
-
-        for symbol in INITIAL_SYMBOLS {
-            positions
-                .entry(symbol.to_string())
-                .or_insert_with(UserPosition::new);
-        }
-
-        UserAccount {
-            balance: 0,
-            locked: 0,
-            positions,
-        }
-    }
-    fn lock_balance(&mut self, amount: Price) {
-        self.balance -= amount;
-        self.locked += amount;
-    }
-
-    fn increase_balance(&mut self, amount: Price) {
-        self.balance += amount;
-    }
-
-    fn consume_locked_balance(&mut self, amount: Price) {
-        self.locked -= amount;
-    }
-}
-
-impl UserPosition {
-    fn new() -> Self {
-        UserPosition {
-            quantity: 0,
-            locked: 0,
-        }
-    }
-    fn lock_position(&mut self, quantity: Quantity) {
-        self.quantity -= quantity;
-        self.locked += quantity;
-    }
-
-    fn increase_position_quantity(&mut self, quantity: Quantity) {
-        self.quantity += quantity;
-    }
-
-    fn consume_locked_position_quantity(&mut self, quantity: Quantity) {
-        self.locked -= quantity;
-    }
-}
-
-#[derive(Debug)]
-enum AccountsError {
-    InvalidPrice,
-    InvalidQuantity,
-    UnknownUser,
-    InsufficientBalance,
-    InsufficientPositionQuantity,
-    InvalidSymbol,
-}
-
-impl AccountsManager {
-    fn new() -> Self {
-        AccountsManager {
-            accounts: HashMap::new(),
-        }
-    }
-
-    fn create_account(&mut self, user_id: UserId) {
-        let user_account = UserAccount::new();
-        self.accounts.insert(user_id, user_account);
-    }
-
-    fn increase_account_balance(
-        &mut self,
-        user_id: UserId,
-        amount: Price,
-    ) -> Result<(), AccountsError> {
-        let user_account = self.get_user_account_mut(user_id)?;
-        user_account.increase_balance(amount);
-        Ok(())
-    }
-
-    fn consume_locked_balance(
-        &mut self,
-        user_id: UserId,
-        amount: Price,
-    ) -> Result<(), AccountsError> {
-        let user_account = self.get_user_account_mut(user_id)?;
-        user_account.consume_locked_balance(amount);
-        Ok(())
-    }
-
-    fn increase_position_quantity_for_symbol(
-        &mut self,
-        user_id: UserId,
-        symbol: Symbol,
-        quantity: Quantity,
-    ) -> Result<(), AccountsError> {
-        let position = self.get_position_for_symbol_mut(user_id, symbol)?;
-        position.increase_position_quantity(quantity);
-        Ok(())
-    }
-
-    fn consume_locked_position_quantity_for_symbol(
-        &mut self,
-        user_id: UserId,
-        symbol: Symbol,
-        quantity: Quantity,
-    ) -> Result<(), AccountsError> {
-        let position = self.get_position_for_symbol_mut(user_id, symbol)?;
-        position.consume_locked_position_quantity(quantity);
-        Ok(())
-    }
-
-    fn insert_account(&mut self, user_id: UserId, account: UserAccount) {
-        self.accounts.insert(user_id, account);
-    }
-
-    fn get_user_account(&self, user_id: UserId) -> Result<&UserAccount, AccountsError> {
-        let Some(user_account) = self.accounts.get(&user_id) else {
-            return Err(AccountsError::UnknownUser);
-        };
-        return Ok(user_account);
-    }
-
-    fn get_user_account_mut(&mut self, user_id: UserId) -> Result<&mut UserAccount, AccountsError> {
-        let Some(user_account) = self.accounts.get_mut(&user_id) else {
-            return Err(AccountsError::UnknownUser);
-        };
-        return Ok(user_account);
-    }
-
-    fn get_balance(&self, user_id: UserId) -> Result<Price, AccountsError> {
-        self.get_user_account(user_id)
-            .map(|account| account.balance)
-    }
-
-    fn get_position_for_symbol(
-        &self,
-        user_id: UserId,
-        symbol: Symbol,
-    ) -> Result<&UserPosition, AccountsError> {
-        let account = self.get_user_account(user_id)?;
-        let Some(position) = account.positions.get(&symbol) else {
-            return Err(AccountsError::InvalidSymbol);
-        };
-        return Ok(position);
-    }
-
-    fn get_position_for_symbol_mut(
-        &mut self,
-        user_id: UserId,
-        symbol: Symbol,
-    ) -> Result<&mut UserPosition, AccountsError> {
-        let account = self.get_user_account_mut(user_id)?;
-        let Some(position) = account.positions.get_mut(&symbol) else {
-            return Err(AccountsError::InvalidSymbol);
-        };
-        return Ok(position);
-    }
-
-    fn get_position_quantity_for_symbol(
-        &self,
-        user_id: UserId,
-        symbol: Symbol,
-    ) -> Result<Quantity, AccountsError> {
-        let position = self.get_position_for_symbol(user_id, symbol)?;
-        Ok(position.quantity)
-    }
-
-    fn is_enough_balance_for_order(&self, order: &Order) -> Result<(), AccountsError> {
-        match order.side {
-            Side::Bid => {
-                let balance = self.get_balance(order.user_id)?;
-                let min_required_balance = order.remaining * order.price;
-
-                if balance >= min_required_balance {
-                    Ok(())
-                } else {
-                    Err(AccountsError::InsufficientBalance)
-                }
-            }
-            Side::Ask => {
-                let position_quantity =
-                    self.get_position_quantity_for_symbol(order.user_id, order.symbol.clone())?;
-                if order.remaining <= position_quantity {
-                    Ok(())
-                } else {
-                    Err(AccountsError::InsufficientPositionQuantity)
-                }
-            }
-        }
-    }
-
-    fn lock_user_balance_for_order(&mut self, order: &Order) -> Result<(), AccountsError> {
-        let user_account = self.get_user_account_mut(order.user_id)?;
-        let amount = order.price * order.remaining;
-        user_account.lock_balance(amount);
-        Ok(())
-    }
-
-    fn lock_user_user_position_for_order(&mut self, order: &Order) -> Result<(), AccountsError> {
-        let position = self.get_position_for_symbol_mut(order.user_id, order.symbol.clone())?;
-        position.lock_position(order.remaining);
-        Ok(())
-    }
-
-    fn settle_fill(&mut self, fill: &Trade) -> Result<(), AccountsError> {
-        let amount = fill.price * fill.quantity;
-
-        match fill.taker_side {
-            Side::Bid => {
-                self.consume_locked_balance(fill.taker_user_id, amount)?;
-                self.increase_position_quantity_for_symbol(
-                    fill.taker_user_id,
-                    fill.symbol.clone(),
-                    fill.quantity,
-                )?;
-                self.consume_locked_position_quantity_for_symbol(
-                    fill.maker_user_id,
-                    fill.symbol.clone(),
-                    fill.quantity,
-                )?;
-                self.increase_account_balance(fill.maker_user_id, amount)?;
-            }
-            Side::Ask => {
-                self.consume_locked_position_quantity_for_symbol(
-                    fill.taker_user_id,
-                    fill.symbol.clone(),
-                    fill.quantity,
-                )?;
-                self.increase_account_balance(fill.taker_user_id, amount)?;
-                self.consume_locked_balance(fill.maker_user_id, amount)?;
-                self.increase_position_quantity_for_symbol(
-                    fill.maker_user_id,
-                    fill.symbol.clone(),
-                    fill.quantity,
-                )?;
-            }
-        }
-
-        Ok(())
-    }
 }
 
 #[derive(Debug)]
@@ -614,7 +355,7 @@ fn main() {
     );
 
     accounts
-        .lock_user_user_position_for_order(&ask_order_1)
+        .reserve_user_position_for_order(&ask_order_1)
         .unwrap();
     let result1 = orderbook.handle_limit_order(ask_order_1).unwrap();
     for fill in &result1.fills {
@@ -622,7 +363,7 @@ fn main() {
     }
 
     accounts
-        .lock_user_user_position_for_order(&ask_order_2)
+        .reserve_user_position_for_order(&ask_order_2)
         .unwrap();
     let result2 = orderbook.handle_limit_order(ask_order_2).unwrap();
     for fill in &result2.fills {
@@ -631,7 +372,7 @@ fn main() {
 
     println!("book before bid -> {:?}", orderbook);
 
-    accounts.lock_user_balance_for_order(&bid_order).unwrap();
+    accounts.reserve_user_balance_for_order(&bid_order).unwrap();
     let result = orderbook.handle_limit_order(bid_order).unwrap();
     for fill in &result.fills {
         accounts.settle_fill(fill).unwrap();
